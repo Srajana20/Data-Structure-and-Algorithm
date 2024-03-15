cae1be3414b58755fb6ef667c4cd1a8fc2fee063
Srajana Naik <136155358+Srajana20@users.noreply.github.com>
2024-03-15 11:24:42 +0530

    Revisit

---
+++ Array/Transpose of Matrix.cpp
@@ -0,0 +1,9 @@
+// Transpose of Matrix without using one more matrix
+
+
+for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix2[i][j]=matrix[j][i];
+                
+            }
+  }
