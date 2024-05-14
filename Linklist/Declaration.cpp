dc74112b5ceb7638ac22d1e320e5a6c6541d04c0
Srajana Naik <136155358+Srajana20@users.noreply.github.com>
2024-05-14 18:40:22 +0530

    Declaration.cpp

---
+++ Linklist/Declaration.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    
+    //constructor
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+int main() {
+    // Write C++ code here
+    Node *node1 = new Node(5);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
+    return 0;
+}
